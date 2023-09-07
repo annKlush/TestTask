ef29d9c383fb33508c9e8409c41a66fe8b551a3e
annKlush <109134951+annKlush@users.noreply.github.com>
2023-09-07 17:21:45 +0300

    add files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,6 @@ public class Main {
         List<String> dateStrings = Arrays.asList(
                 "2005-07-01", "2005-01-02", "2005-01-01", "2005-05-03"
         );
-        Comparators.compareDate(dateStrings);
+        System.out.println(Comparators.compareDate(dateStrings));
     }
 }
